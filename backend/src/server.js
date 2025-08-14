ed9885ebd599093544a5375e20d16111b84becc0
carlson dultra <carlsondultra@gmail.com>
2025-08-14 19:23:41 -0400

    adding env variables as config file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { ENV } from "./config/env.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is up and running on PORT:5001"));
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:5001"));
 
